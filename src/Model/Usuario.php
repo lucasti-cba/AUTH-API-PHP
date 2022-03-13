e151f0c462f924a83bb8dd6d348dfdabe41954e1
Lucas A. Batista <lucasti-cba@outlook.com>
2022-03-13 12:48:56 -0400

    :white_check_mark: get/set Telefone Celular

--- src/Model/Usuario.php
+++ src/Model/Usuario.php
@@ -27,14 +27,34 @@ class Usuario
     {
         return $this->nome;
     }
-
     public function setNome($nome)
     {
         $this->nome = $nome;
+    }    
+    public function getEmail()
+    {
+        return $this->email;
     }
-
-    
-
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    public function getTelefoneFixo()
+    {
+        return $this->telefone_fixo;
+    }
+    public function setTelefoneFixo($telefone_fixo)
+    {
+        $this->telefone_fixo = $telefone_fixo;
+    }
+    public function getTelefoneCelular()
+    {
+        return $this->telefone_celular;
+    }
+    public function setTelefoneCelular($telefone_celular)
+    {
+        $this->telefone_celular = $telefone_celular;
+    }
 
 
 }
