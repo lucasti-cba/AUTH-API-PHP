2b13f76399bb573fc9d01585b03bd9bb7b64cc38
Lucas A. Batista <lucasti-cba@outlook.com>
2022-03-13 12:54:54 -0400

    :white_check_mark: get/set usuario :white_check_mark: get/set senha

--- src/Model/Usuario.php
+++ src/Model/Usuario.php
@@ -55,6 +55,24 @@ class Usuario
     {
         $this->telefone_celular = $telefone_celular;
     }
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function getSenha()
+    {
+       return $this->senha;
+    }
+    
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
 
 
 }
